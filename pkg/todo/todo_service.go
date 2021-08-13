956a2654a3d528f2761c4dfaeb49f1ea3e32aeaa
djumanoff@gmail.com <idris@Idriss-MacBook-Pro.local>
2021-08-13 09:03:22 +0600

    add todo service

--- pkg/todo/todo_service.go
+++ pkg/todo/todo_service.go
@@ -6,6 +6,35 @@ type Service interface {
 	Done(id int64) error
 }
 
-type svc struct {
-	Repository
+func NewService(repo Repository) Service {
+	return &defaultSvc{repo}
+}
+
+type defaultSvc struct {
+	repo Repository
+}
+
+func (svc *defaultSvc) NewTodo(title string, body string) (*Todo, error) {
+	todo := &Todo{}
+	todo.Title = title
+	todo.Body = body
+	todo.Status = "todo"
+	id, err := svc.repo.Create(todo)
+	if err != nil {
+		return nil, err
+	}
+	todo.ID = id
+	return todo, nil
+}
+
+func (svc *defaultSvc) Done(id int64) error {
+	status := "done"
+	upd := &TodoUpdate{}
+	upd.ID = id
+	upd.Status = &status
+	err := svc.repo.Update(upd)
+	if err != nil {
+		return err
+	}
+	return nil
 }
